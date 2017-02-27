2beacd6adeb850811cc935725e101bed8c5088c1
musher1720 <mushbuttons2@gmail.com>
2017-02-27 11:46:02 +0800

    skrip2

--- gettingdata.R
+++ gettingdata.R
@@ -1,3 +1,3 @@
-if(!file.exists(".\GettingData")) {
-  dir.create(".\GettingData")
-}
+library(readr)
+bptms_Labour_force_by_marital_status_state <- read_csv("C:/Users/MOE/Downloads/bptms-Labour_force_by_marital_status_state.csv")
+View(bptms_Labour_force_by_marital_status_state)
